a069a288c3e0e12f6541e453b3ae18fa283f90ca
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-07-29 23:57:58 -0400

    added friend images

--- src/App.js
+++ src/App.js
@@ -13,20 +13,51 @@ class App extends Component {
     highScore: 0
   };
 
+  handleFriendClick = id => {
+    if (this.state.chosenFriend.includes(id)) {
+      alert("You have already chosen that friend. Start again!")
+      this.handleReset();
+    } else {
+      let idArray = [];
+      idArray.push(id);
+      this.setState({ chosenFriend: idArray});
+      this.state.currentScore <= this.state.highScore -1 ? this.setState({ currentScore: this.state.currentScore +1 })
+      : this.setState({
+        currentScore: this.state.currentScore + 1,
+        highScore: this.statehighScore + 1
+      });
+
+      let friendCardShuffle = this.state.friends;
+      for (let i = friendCardShuffle.length -1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [friendCardShuffle[i], friendCardShuffle[j]] = [
+          friendCardShuffle[j], friendCardShuffle[i]
+        ];
+      }
+      this.setState({ friends: friendCardShuffle})
+    }
+  };
+
+  handleReset = () => {
+    this.setState ({ chosenFriend: [], currentScore: 0 });
+  };
+
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <Title
+          currentScore={this.state.currentScore}
+          highScore={this.state.highScore}
+          />
+
         {this.state.friends.map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
+            handleFriendClick={this.handleFriendClick}
             id={friend.id}
             key={friend.id}
             name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
           />
         ))}
       </Wrapper>
