bf8f1d8a5d866bd6a92063bf6cb7d0139cc49e19
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-07-29 23:22:44 -0400

    initial file setup

---
+++ src/components/Title/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Title(props) {
+  return (
+    <div className="scorebar">
+    <nav className="navbar-nav fixed-top">
+    <ul className="navbar">
+      <li className="navbar-brand" id="game-name">Golden Girls Memory Game</li>
+      <li className="navbar-brand" id="game-result">Click an Image to Start Game</li>
+      <li className="navbar-brand" id="game-score">Score: {props.currentScore} | Top Score: {props.highScore}</li>
+    </ul></nav>
+    </div>
+  );
+}
+
+export default Title;
