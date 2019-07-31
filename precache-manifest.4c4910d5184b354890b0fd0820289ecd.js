1fb7e6ed6291b6f6ae9336d5cb8298642700a259
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-07-30 22:51:09 -0400

    Updates

---
+++ precache-manifest.4c4910d5184b354890b0fd0820289ecd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b597d0988a5649861b3de4ff14b99f",
+    "url": "/Clicky-Game-React-App/index.html"
+  },
+  {
+    "revision": "f6440927701e1803888d",
+    "url": "/Clicky-Game-React-App/static/css/main.9016ba56.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Game-React-App/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f6440927701e1803888d",
+    "url": "/Clicky-Game-React-App/static/js/main.66b68ed7.chunk.js"
+  },
+  {
+    "revision": "3be2621c74ae69ee313c",
+    "url": "/Clicky-Game-React-App/static/js/runtime~main.5ca89a0d.js"
+  }
+]);
